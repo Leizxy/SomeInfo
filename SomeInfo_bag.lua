4f03e1fda391c6b5e352fc013e4b8311dc5238c5
Leizxy <359844413@qq.com>
2016-08-29 23:02:29 +0800

    update

--- SomeInfo_bag.lua
+++ SomeInfo_bag.lua
@@ -140,3 +140,32 @@ function SlashCmdList.JUSTSELL(msg,editbox)
 	end
 end
 
+--[自动修理] 
+local AutoRepair = true 
+local g = CreateFrame("Frame") 
+g:RegisterEvent("MERCHANT_SHOW") 
+g:SetScript("OnEvent", function()     
+   if(AutoRepair==true and CanMerchantRepair()) then 
+           local cost = GetRepairAllCost() 
+      if cost > 0 then 
+         local money = GetMoney() 
+         if IsInGuild() then 
+            local guildMoney = GetGuildBankWithdrawMoney() 
+            if guildMoney > GetGuildBankMoney() then 
+               guildMoney = GetGuildBankMoney() 
+            end 
+            if guildMoney > cost and CanGuildBankRepair() then 
+               RepairAllItems(1) 
+               print(format("|cfff07100工会修理花费: %.1fg|r", cost * 0.0001)) 
+               return 
+            end 
+         end 
+         if money > cost then 
+               RepairAllItems() 
+               print(format("|cffead000修理花费: %.1fg|r", cost * 0.0001)) 
+         else 
+            print("Go farm newbie.") 
+         end 
+      end 
+	end 
+end)
--- SomeInfo_coords.lua
+++ SomeInfo_coords.lua
@@ -4,18 +4,20 @@ local coords = CreateFrame("Frame",nil,UIParent)
 coords:EnableMouse(true)
 local coords_Text = coords:CreateFontString(nil,"OVERLAY")
 coords_Text:SetFont(unpack(info.Font))
+coords_Text:SetShadowOffset(1,-1)
+coords_Text:SetShadowColor(0, 0, 0, 0.5)
 info.Frames["coords"] = coords_Text
 
 coords:SetAllPoints(coords_Text)
 
 local coordsX,coordsY = 0,0
-local function formatCoords() return format("%d,%d",coordsX*100,coordsY*100) end
-local function formatCoordsFloat() return format("%.1f,%.1f",coordsX*100,coordsY*100) end
+local function formatCoords() return format("(|cff0CD809%d|r,|cff0CD809%d|r)",coordsX*100,coordsY*100) end
+local function formatCoordsFloat() return format("(|cff0CD809%.1f|r,|cff0CD809%.1f|r)",coordsX*100,coordsY*100) end
 
-coords:SetScript("OnUpdate",function()
+coords:SetScript("OnUpdate",function(self)
 	coordsX,coordsY = GetPlayerMapPosition("player")
 	if self:IsMouseOver() then 
-		coords_Text:SetText(formatCoordsFloat())
+		-- coords_Text:SetText(formatCoordsFloat())
 	else
 		coords_Text:SetText(formatCoords())
 	end
@@ -33,7 +35,10 @@ WorldMapButton:HookScript("OnUpdate", function(self)
 	if not self.coordText then 
 		--self.coordText = WorldMapFrameCloseButton:CreateFontString(nil, "OVERLAY", "GameFontGreen") 
 		self.coordText = WorldMapFrameCloseButton:CreateFontString(nil, "OVERLAY", "GameFontWhite") 
-		self.coordText:SetPoint("BOTTOM", self, "BOTTOM", 0, 6) 
+		self.coordText:SetFont(UNIT_NAME_FONT, 14, "THINOUTLINE")
+		self.coordText:SetPoint("BOTTOM", WorldMapScrollFrame, "BOTTOM", 0, 6) 
+		self.coordText:SetShadowOffset(1,-1)
+		self.coordText:SetShadowColor(0, 0, 0, 1)
 	end 
 	local px, py = GetPlayerMapPosition("player") 
 	local x, y = GetCursorPosition() 
@@ -43,10 +48,10 @@ WorldMapButton:HookScript("OnUpdate", function(self)
 	if px == 0 and py == 0 and (x > 1 or y > 1 or x < 0 or y < 0) then 
 		self.coordText:SetText("") 
 	elseif px == 0 and py == 0 then 
-		self.coordText:SetText(format("mouse: |cff0CD809%.1f, %.1f|r", x*100, y*100)) 
+		self.coordText:SetText(format("Mouse: |cff0CD809%.1f, %.1f|r", x*100, y*100)) 
 	elseif x > 1 or y > 1 or x < 0 or y < 0 then 
-		self.coordText:SetText(format("player: |cff0CD809%.1f, %.1f|r", px*100, py*100)) 
+		self.coordText:SetText(format("You: |cff0CD809%.1f, %.1f|r", px*100, py*100)) 
 	else 
-		self.coordText:SetText(format("player: |cff0CD809%.1f, %.1f|r; mouse: |cff0CD809%.1f, %.1f|r", px*100, py*100, x*100, y*100)) 
+		self.coordText:SetText(format("You: |cff0CD809%.1f, %.1f|r; Mouse: |cff0CD809%.1f, %.1f|r", px*100, py*100, x*100, y*100)) 
 	end 
 end)
